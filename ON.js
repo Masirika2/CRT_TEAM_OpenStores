76788f2f610a7e922b9dbb9ccb516c9a4c9728bc
Masirika2 <fungulomasirika8@gmail.com>
2023-11-07 08:06:09 +0600

    Update ON.js

--- ON.js
+++ ON.js
@@ -2,13 +2,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const generateOrderNumberButton = document.getElementById("generateOrderNumber");
     const orderNumberSpan = document.getElementById("orderNumber");
 
-    generateOrderNumberButton.addEventListener("click", function () {
+    generateOrderNumberButton.addEventListener("click", async function () {
         const orderNumber = generateOrderNumber();
         orderNumberSpan.textContent = orderNumber;
+
+        try {
+            const response = await fetch('/orders', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ orderNumber })
+            });
+
+            if (response.ok) {
+                console.log('Order stored successfully:', orderNumber);
+            } else {
+                console.error('Failed to store the order:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error storing order:', error);
+        }
     });
 
     function generateOrderNumber() {
-        // Generate a random order number 
         return "ORD-" + Math.floor(Math.random() * 10000);
     }
 });
